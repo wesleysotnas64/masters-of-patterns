de40a87a4b735c5538fbfd4efb767f0e8433fa36
Wesley Santos <wesleysotnas64@gmail.com>
2024-06-10 20:52:17 -0300

    Update do Projeto.

---
+++ masters-of-patterns/Assets/Scripts/QuizCanvasController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizCanvasController : MonoBehaviour
+{
+    public TMP_Text textQuestion;
+    public Image imgHealthPlayer;
+    public Image imgHealthPlayerEffect;
+    public List<GameObject> listOptionButton;
+    public List<TMP_Text> listTextOption;
+    public Button btnNext;
+
+    public Question currentQuestion;
+    public bool skipping;
+    public float delay;
+    public List<int> idsAlreadyDrawn;
+    public int qtdQuestions;
+
+    //Player
+    public float playerHealth;
+    public float playerHealthCurrent;
+    public float playerDamage;
+
+    //Monster
+    public float monsterHealth;
+    public float monsterHealthCurrent;
+    public float monsterDamage;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
